e0a7748a71667c713776f5eb8c010a99404dba8e
Miguel Yuste <yusaguilar@gmail.com>
2023-04-23 21:13:41 +0200

    236. Métodos de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -30,6 +30,25 @@ export class BasicPageComponent implements OnInit {
     // this.myForm.reset(rtx5090);
   }
 
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors &&
+      this.myForm.controls[field].touched;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {}
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+    return null;
+  }
+
   onSave(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
