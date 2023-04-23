c882617f8354b3d4ad71d1a1995b906c8d021045
Miguel Yuste <yusaguilar@gmail.com>
2023-04-23 22:02:52 +0200

    238. Eliminar elementos de un FormArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -18,6 +18,10 @@ export class DynamicPageComponent {
   get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray;
   }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
   
   onSubmit(): void {
     if (this.myForm.invalid) {
