27af8c1e50685d09507e43801e9f64012fe9d2cb
Miguel Yuste <yusaguilar@gmail.com>
2023-04-23 20:51:56 +0200

    235. Mostrar errores de validación en pantalla

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -27,11 +27,14 @@ export class BasicPageComponent implements OnInit {
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.myForm.reset(rtx5090);
+    // this.myForm.reset(rtx5090);
   }
 
   onSave(): void {
-    if (this.myForm.invalid) return;
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    };
     console.log(this.myForm.value);
     this.myForm.reset({ price: 0, inStorage: 0 });
   }
