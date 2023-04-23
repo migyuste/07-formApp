40d0d9054039651a3208d57bbfa6faef627892cb
Miguel Yuste <yusaguilar@gmail.com>
2023-04-23 12:27:59 +0200

    230. Rutas de nuestra aplicación

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'sign-up', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'sign-up' },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
