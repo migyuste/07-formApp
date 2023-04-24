d623cbeec1d55bd3e9acf5ef1a5ca6c449696e7f
Miguel Yuste <yusaguilar@gmail.com>
2023-04-24 19:04:00 +0200

    244. Continuación de proyecto y tarea

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SwitchesPageComponent {
-
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  })
+  constructor(private fb: FormBuilder) { }
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  } 
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset({gender: 'M', wantNotifications: true, termsAndConditions: false} )
+  }
 }
