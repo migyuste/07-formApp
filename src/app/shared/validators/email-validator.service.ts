645fa39c44f623ba386c3c2ed39c1b353ea16f7c
Miguel Yuste <yusaguilar@gmail.com>
2023-04-25 20:14:02 +0200

    251. Elaborar el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -8,10 +8,23 @@ import { Observable, delay, of } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class EmailValidatorService implements AsyncValidator {
+  //   validate(control: AbstractControl): Observable<ValidationErrors | null> {
+  //     const email = control.value;
+  //     console.log({ email });
+
+  //     return of({ emailTaken: true }).pipe(delay(3000));
+  //   }
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
     const email = control.value;
-    console.log({ email });
-
-    return of({ emailTaken: true }).pipe(delay(3000));
+      const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+          console.log({ email });
+          if (email === 'yusaguilar@gmail.com') {
+              subscriber.next({ emailTaken: true });
+              subscriber.complete();
+          }
+          subscriber.next(null);
+          subscriber.complete();
+      }).pipe(delay(3000));
+      return httpCallObservable;
   }
 }
